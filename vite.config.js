06fc3c0986f38ada0998e452dd78f86f4f5fd590
ykkim97 <97ykkim@naver.com>
2024-06-20 17:31:36 +0900

    config : proxy 설정 삭제

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_BACKEND_API_URL,
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
